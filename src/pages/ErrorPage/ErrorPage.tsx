548849c252394ac66598a1fcf2815ecb3e7662b1
TatyanaPanchenko-web <web.tatyanapanchenko@gmail.com>
2026-01-06 14:28:59 +0300

    Fix images path

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -6,7 +6,7 @@ export default function ErrorPage() {
       <div className={style["error-title"]}> Такой страницы нет</div>
 
       <div className={style["error-img"]}>
-        <img src="/src/assets/icons/pizza.png" alt="pizza"></img>
+        <img src="/icons/pizza.png" alt="pizza"></img>
       </div>
     </div>
   );
