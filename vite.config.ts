6be76ae550fcf8a2bcfac267f281c5c572efe016
TatyanaPanchenko-web <web.tatyanapanchenko@gmail.com>
2026-01-06 12:54:39 +0300

    Fix sass import

--- vite.config.ts
+++ vite.config.ts
@@ -52,8 +52,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "~": path.resolve(__dirname, "./assets"),
+      "@": path.resolve(__dirname, "src"),
+      "~": path.resolve(__dirname, "assets"),
     },
   },
 });
