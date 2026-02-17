f1430c68b2dc9453fd0c9b760362ca524a7074cb
TatyanaPanchenko-web <web.tatyanapanchenko@gmail.com>
2026-02-17 20:17:17 +0300

    Add lazy loading in app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,16 @@
+import React, { Suspense } from "react";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import {
-  RegistrationPage,
-  AuthorizationPage,
-  MainPage,
-  ErrorPage,
-} from "@/pages";
 import { useGetAuthData } from "@/bll/useGetAuthData";
+
+const RegistrationPage = React.lazy(() => import("@/pages/RegistrationPage/RegistrationPage"));
+const AuthorizationPage = React.lazy(() => import("@/pages/AuthorizationPage/AuthorizationPage"));
+const MainPage = React.lazy(() => import("@/pages/MainPage/MainPage"));
+const ErrorPage = React.lazy(() => import("@/pages/ErrorPage/ErrorPage"));
+
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Loader from "@/components/Loader/Loader";
 import style from "./app.module.scss";
 
 export default function App() {
@@ -18,18 +20,20 @@ export default function App() {
   return (
     <div className={style.app}>
       {showHeader && <Header />}
-      <Routes>
-        <Route path="/" element={<MainPage dataAuth={userInfo} />} />
-        <Route
-          path="/authorization"
-          element={<AuthorizationPage setShowHeader={setShowHeader} />}
-        ></Route>
-        <Route
-          path="/registration/*"
-          element={<RegistrationPage setShowHeader={setShowHeader} />}
-        ></Route>
-        <Route path="*" element={<ErrorPage />}></Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<MainPage dataAuth={userInfo} />} />
+          <Route
+            path="/authorization"
+            element={<AuthorizationPage setShowHeader={setShowHeader} />}
+          ></Route>
+          <Route
+            path="/registration/*"
+            element={<RegistrationPage setShowHeader={setShowHeader} />}
+          ></Route>
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
